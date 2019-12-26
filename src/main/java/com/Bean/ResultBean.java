df4e04048a7f50e5b6c187ffcd725022854acc3e
zhangantai <zhangantai@qq.com>
2019-12-26 10:26:48 +0800

    创建新类

---
+++ src/main/java/com/Bean/ResultBean.java
@@ -0,0 +1,18 @@
+package com.Bean;
+
+/**
+ * @Auther: ZAT
+ * @Date: 2019/12/26 10:21
+ * @Description:
+ */
+public class ResultBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
